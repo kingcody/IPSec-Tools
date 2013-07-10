91c624a03e553248dde6c6c3c8deb3979c143968
kingcody <me@codymize.com>
2013-07-10 10:33:02 -0400

    adding base project

---
+++ IPSec-Tools.js
@@ -0,0 +1,20 @@
+/*
+ * Node.JS Module for managing IPSec VPN's 
+ */
+
+// Module Imports
+var ipTools	= require('./lib/ip-tools.js'),
+	setkey	= require('./lib/setkey.js');
+
+// Contructor for IPSecTools
+var IPSecTools = function IPSecTools() {
+
+};
+
+// Prototypically declaring IPSecTools' methods
+IPSecTools.prototype = {
+	ip: ipTools,
+	setkey: setkey
+};
+
+module.exports = new IPSecTools();
